f61b815d91e1ac7d5cfc6a2de87f44982741b8f0
convivial-system-4430 <cdneal091@gmail.com>
2019-03-03 02:26:39 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,6 @@
 def oxford_comma(array)
-
+  return array[0] while array.size < 2
+  return "#{array[0]} and #{array[1]}" while array.size == 2
+  last = array.pop
+  array.join(", ") << ", and #{last}"
 end
